826610a8b8f86e21f3ba528f1b79aaa1249d22df
julianbooher <julianbooher@hotmail.com>
2020-11-09 16:15:42 -0600

    everything sourced in, able to append photos to the DOM

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('in jQuery');
+    displayPhotos();
+    $('photo').on('click', )
+}
+
+function displayPhotos(){
+    for (let x of people){
+        $('.photos').append(`
+            <div class="photo" data-name="${x.name}">
+            <img src="https://github.com/${x.githubUsername}.png?size=250"
+            </div>
+        `)
+    }
+}
