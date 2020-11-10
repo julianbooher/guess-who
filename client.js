bf2c4867661502455a4a163e878aa580c2bf4dd0
julianbooher <julianbooher@hotmail.com>
2020-11-09 22:26:44 -0600

    everything working but shuffle not, may remove

--- client.js
+++ client.js
@@ -5,7 +5,6 @@ let currentGuessWho;
 
 function onReady(){
     console.log('in jQuery');
-    displayPhotos();
     displayGuessWho();
     $('.photos').on('click', '.photo', select);
     $('.alert-message').on('click', '.new-name', displayGuessWho);
@@ -15,6 +14,7 @@ function displayPhotos(){
     // Emptying for potential later feature where they get randomized eached turn.
     $('.photos').empty();
     // Loop through people array and display their image to the DOM.
+    people = shuffle(people);
     for (let x of people){
         $('.photos').append(`
             <div class="photo-div">
@@ -27,22 +27,29 @@ function displayPhotos(){
 function select(){
     // Get the github username of the img the user clicks on.
     let github = $(this).data("github");
+    let currentImage = this;
     // Compare the current guessWho username to the one the user clicked on.
     // If they match, do success things!
     if (currentGuessWho.githubUsername === github){
         $('.alert-message').empty();
         $('.alert-message').append('<h2 class="success">SUCCESS!</h2>');
         $('.alert-message').append('<br><button class="new-name">Get a New Name</button');
+        $(currentImage).toggleClass('blue-border');
+        setTimeout(function(){$(currentImage).toggleClass('blue-border')}, 2000)
     } 
     // If they don't match, do failure things!
     else {
         $('.alert-message').empty();
         $('.alert-message').append('<h2 class="wrong">SORRY!<br> Try again.</h2>')
+        $(currentImage).toggleClass('red-border');
+        setTimeout(function(){$(currentImage).toggleClass('red-border')}, 2000)
     }
     console.log(github);
 }
 
 function displayGuessWho(){
+    // Display the photos.
+    displayPhotos();
     // Get random number that will represent the index in the people array.
     let x = randomNumber(0, people.length);
     // If there is something in the guess-who div, empty it.
@@ -50,6 +57,7 @@ function displayGuessWho(){
     // If there is an alert-message and button from a successful guess, empty it.
     $('.alert-message').empty();
     // Post the name that the user should be guessing.3
+    console.log(people);
     $('.guess-who').append(`<h3>Which picture is ${people[x].name}?</h3>`)
     currentGuessWho = people[x];
     console.log('currentGuessWho:', currentGuessWho.name)
@@ -59,3 +67,26 @@ function displayGuessWho(){
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+// Algorithm to shuffle an array into a new, random order. Sources provided.
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// https://bost.ocks.org/mike/shuffle/
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
